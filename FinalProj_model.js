cb6922874cd0bade10b5ac66159b60b0cf92ca22
EitanCher <102179966+EitanCher@users.noreply.github.com>
2023-06-28 00:38:06 +0300

    Update FinalProj_model.js

--- FinalProj_model.js
+++ FinalProj_model.js
@@ -7,23 +7,23 @@ const dataSchema = new mongoose.Schema({
     },
     phase1_duration: {
         required: true,
-        type: Number,
-        min: 2
+        type: Number/*,
+        min: 2*/
     },
     phase2_duration: {
         required: true,
-        type: Number,
-        min: 2
+        type: Number/*,
+        min: 2*/
     },
     phase3_duration: {
         required: true,
-        type: Number,
-        min: 2
+        type: Number/*,
+        min: 2*/
     },
     phase4_duration: {
         required: true,
-        type: Number,
-        min: 2
+        type: Number/*,
+        min: 2*/
     }
 })
 
