418d98352523525f7633d111f895e1695fcf38a1
EitanCher <102179966+EitanCher@users.noreply.github.com>
2023-06-28 10:01:29 +0300

    Update FinalProj.ino
    Dynamic variables assignments enabled

--- FinalProj/FinalProj.ino
+++ FinalProj/FinalProj.ino
@@ -1,9 +1,19 @@
 #define pinRelay D4
 #define pinLDR A0
 
-bool isPhase1, isPhase2, isPhase3, isPhase4;
-String phase1 = "1", phase2 = "2", phase3 = "3", phase4 = 4;
-int phase1_duration, phase2_duration, phase3_duration, phase4_duration;
+//bool isPhase1, isPhase2, isPhase3, isPhase4;
+//int phase1_duration, phase2_duration, phase3_duration, phase4_duration;
+
+short NumOfPhases = 4, MinDuration = 2, CurrentPhase, NextPhase;
+
+//For each phase: 2 parameters (on/off and duration)
+//Amount is "+1" for better code-readability, ignore the zero
+short MyVars = new short[NumOfPhases + 1][2];  
+for(short i = 1; i <= NumOfPhases; i++) {
+  myVars[i][0] = 0;  // Phase off
+  myVars[i][1] = MinDuration;
+}
+
 unsigned long myTimer = millis();
 
 long int  ph2_val;  // Light measurement for Phase 2. "Long" for case of long duration of Phase 2.
@@ -14,6 +24,9 @@ int       ph3_output; // PWM value provided on Phase 3
 
 void setup() {
   pinMode(pinRelay, OUTPUT);  // Relay control pin
+  isPhase1 = true;  // Enable Phase 1 on program start
+  // or: myVars[1][0] = 1;  // Enable Phase 1 on program start
+  
   Serial.begin(9600);
   Serial.println("Started ...");
 
@@ -21,19 +34,27 @@ void setup() {
 }
 
 void loop() {
-  if (isPhase1) {
-    phase1_duration = GetDuration(phase1); 
+  // Phase 1 ======================================================
+  if(myVars[1][0] == 1) {  
+    //Derive the duration value, set current/next variables:
+    PhaseSettings(1);
 
+    //Phase staff:
     digitalWrite(pinRelay, LOW);  // Turn the relay on
-    MovePhase(phase1_duration, isPhase1, isPhase2);
+
+    //Disable current phase, enable next phase (when time off):
+    NextPhase(myVars[CurrentPhase][1], CurrentPhase, NextPhase);
   }
   else {
     digitalWrite(pinRelay, HIGH); // Turn the relay off
   }
 
-  if (isPhase2) {
-    phase2_duration = GetDuration(phase2);
+  // Phase 2 ======================================================
+  if(myVars[2][0] == 1) {  
+    //Derive the duration value, set current/next variables:
+    PhaseSettings(2);
 
+    //Phase staff:
     if (ph2_bool) {
       ph2_val = ph2_val + analogRead(pinLDR); // Perform measurement, add its value to the sum of previous measurements
       ph2_avg = ph2_val / ++ph2_cnt;          // Calculate the average
@@ -46,34 +67,50 @@ void loop() {
       }
     }
 
-    MovePhase(phase2_duration, isPhase2, isPhase3);
+    //Disable current phase, enable next phase (when time off):
+    NextPhase(myVars[CurrentPhase][1], CurrentPhase, NextPhase);
   }
 
-  if (isPhase3) {
-    phase3_duration = GetDuration(phase3); 
+  // Phase 3 ======================================================
+  if(myVars[3][0] == 1) {
+    //Derive the duration value, set current/next variables:
+    PhaseSettings(3);
+
+    //Phase staff:
     ph3_output = map(ph2_avg, 0, 1023, 0, 255); // Provide a PWM value for external device
 
-    MovePhase(phase3_duration, isPhase3, isPhase4);
+    //Disable current phase, enable next phase (when time off):
+    NextPhase(myVars[CurrentPhase][1], CurrentPhase, NextPhase);
   }
 
-  if (isPhase4) {
-    phase4_duration = GetDuration(phase4); 
+  // Phase 4 ======================================================
+  if(myVars[4][0] == 1) {
+    //Derive the duration value, set current/next variables:
+    PhaseSettings(4);
 
-    // Reset all the variables before next cicle begins:
+    //Phase staff:
+    //Reset all the variables before next cicle begins:
     ph2_val = 0;
     ph2_cnt = 0;
     ph2_avg = 0;
     ph2_bool = true;
     ph3_output = 0;
 
-    MovePhase(phase4_duration, isPhase4, isPhase1);
+    //Disable current phase, enable next phase (when time off):
+    NextPhase(myVars[CurrentPhase][1], CurrentPhase, NextPhase);
   }
 }
 
-void MovePhase(int& duration, bool& current, bool& next){
+void NextPhase(short duration, short current, short next){
   if (millis() - myTimer >= (duration * 1000) {
-    current = false;
-    next = true;
+    MyVars[current][0] = 0;
+    MyVars[next][0] = 1;
     myTimer = millis();
   }
+}
+
+void PhaseSettings(short MyPhase){
+  CurrentPhase = MyPhase;
+  NextPhase = ++MyPhase;
+  myVars[CurrentPhase][1] = GetDuration(CurrentPhase); 
 }
