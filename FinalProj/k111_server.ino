4aaf4677fd1838319c7785dc8dfeec32d11b4463
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2023-06-29 21:53:16 +0300

    Update k111_server.ino

--- FinalProj/k111_server.ino
+++ FinalProj/k111_server.ino
@@ -3,15 +3,15 @@
 #include <WiFiUdp.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "FinalProj";
-//const char* pswd = "55555333";
+const char* ssid = "yyyy";
+const char* pswd = "6666";
 
 WiFiClient client;
 int server_port = 80;
 
 void wifi_Setup() {
   Serial.println("wifiSetup");
-  WiFi.begin(ssid);
+  WiFi.begin(ssid, pswd);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Attempting to connect to network...");
@@ -20,18 +20,21 @@ void wifi_Setup() {
   Serial.println("Connected to network");
 }
 
-int GetDuration(short PhaseNum) {
-  string MyInput = to_string(PhaseNum);
+int GetDuration(int PhaseNum) {
+  int ret = -1;
   HTTPClient http;
-  http.begin(client, "http://77.137.39.113:6655/R/" + MyInput);
+  
+  String MyInput = String(PhaseNum);
+  http.begin(client, "http://192.168.1.17:6655/R/" + MyInput);
   int httpCode = http.GET();
   Serial.println(httpCode);
+
   if (httpCode == HTTP_CODE_OK) {
     Serial.print("HTTP response code ");
     Serial.println(httpCode);
     String Res = http.getString();
     Serial.println(Res);
-    int ret = Res.toInt();
+    ret = Res.toInt();
   }
   http.end();
 
